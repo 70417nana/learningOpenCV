e4a1c2a13e684782cab6854af6d57b286390f54f
Nana <70417nana@gmail.com>
2021-01-16 15:00:28 +0900

    反映が遅いのと一回目に表示されない問題を修正

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ cv2.createTrackbar('th_max','image',15,255,nothing)
 beforemed= 5
 before1 = 5
 before2 = 15
+firestflag = 1
 
 while(1):
     k = cv2.waitKey(1) & 0xFF
@@ -31,21 +32,24 @@ while(1):
     print(thre2)
     print(medksize)
 
-    if before1 != thre1 or before2 != thre2 or beforemed != medksize:
+    if before1 != thre1 or before2 != thre2 or beforemed != medksize or firestflag == 1:
         before1 = thre1
         before2 = thre2
+        beforemed = medksize
+        firestflag = 0
 
         # median
         img_med = cv2.medianBlur(img, medksize)
 
         #detect edge
         edges = cv2.Canny(img_med,thre1,thre2)
+        mededges = cv2.medianBlur(edges, 1)
         # edges = cv2.Canny(img,5,15)
 
         #edge
         plt.subplot(121),plt.imshow(img,cmap = 'gray')
         plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-        plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+        plt.subplot(122),plt.imshow(mededges,cmap = 'gray')
         plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
 
         plt.show()
